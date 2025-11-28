3675f3527dad94ddd92fbfbd1f758bc94fcdf1af
Patel Jay <52645319+dearjay22@users.noreply.github.com>
2025-11-28 17:40:04 -0500

    Delete cdk.out directory

--- cdk.out/asset.f448b6f09f84fde12649bfd7bcdb73e64a3e4df9176cbd06d128031aadd3617d/handler.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": '{"message": "Hello from Lambda!"}',
-        "headers": {"Content-Type": "application/json"}
-    }
